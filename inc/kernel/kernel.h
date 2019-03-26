bcd514935146c410cf710c74aefd78792216faed
Makshanov Oleg <pvp@dcconsult.ru>
2019-03-26 12:42:30 +0300

    Add kernel panic, unit tests, task priority sort

--- inc/kernel/kernel.h
+++ inc/kernel/kernel.h
@@ -1,5 +1,19 @@
+/********************************************************************************
+ Общий заголовочный файл ядра системы.
+
+
+
+ Проект:      Neocore                                                         
+ Автор:       Макшанов Олег Васильевич                                     
+ Дата:        14 марта 2019г                                               
+ Версия:      0.1                                                          
+ Компания:    ООО "ДиСиСи"                                                 
+ mail:        pvp@dcconsult.ru 
+ ********************************************************************************/
 
 #include "kernel/kernel_configuration.h"
+#include "kernel/kcodes.h"
+#include "kernel/kevents.h"
 #include "stdint.h"
 #include "stdbool.h"
 
@@ -8,33 +22,15 @@
 // Определения типов
 //********************************
 
-typedef enum kcodes // Коды возврата функций ядра
-{
-    k_ok = 0x00,
-    k_noevents = 0x01,
-    k_nosubscribe = 0x02,
-    k_task_list_empty = 0x03
-} kcodes;
-
-
-typedef uint8_t k_event; // Событие ядра
 typedef kcodes (*task_hndl)(void); // Обработчик задачи
-
-// Перечень событий в системе. Все возможные события должны
-// быть перечисленныу в перечислении. Компиляция событий статическая
-// для ускорения работы
-typedef enum event_id
-{
-    event_booted,
-    EVENT_LASTEVENT_IN_ENUM // ВСЕГДА ПОСЛЕДНЕЕ В ENUM! 
-} event_id;
 
 
 typedef enum task_priority // Перечень приоритетов задач
 {
-    high_priority = 0,
+    hw_priority = 255,
+    high_priority = 200,
     middle_priority = 50,
-    low_prioritty = 100,
+    low_prioritty = 0,
 } task_priority;
 
 
